9788efaf5b279ce6e56c82cfffd5efb4977cb888
djghis <djghis@hotmail.com>
2020-01-24 15:45:58 +0000

    3 passes Songs class

--- specs/songs_spec.rb
+++ specs/songs_spec.rb
@@ -9,8 +9,20 @@ require_relative('../songs')
 class TestSongs< Minitest::Test
 
   def setup
-
+    @song = Songs.new("Rockstar", 2.23, "Rock")
   end
 
+
+  def test_has_title
+      assert_equal("Rockstar", @song.title)
+    end
+
+    def test_has_length
+      assert_equal(2.23, @song.length)
+    end
+
+    def test_has_style
+      assert_equal("Rock", @song.style)
+    end
 
 end
